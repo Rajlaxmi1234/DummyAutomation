599a20c086ff0fea18cc74512ab307c7473ac4c5
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-04 11:44:40 +0530

    commit

--- DummyMaven_10/src/test/java/com/bindu/PracticeTest.java
+++ DummyMaven_10/src/test/java/com/bindu/PracticeTest.java
@@ -3,7 +3,7 @@ package com.bindu;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Practice {
+public class PracticeTest {
 @Test
 public void pillAndForkReguest() {
 	Reporter.log("please add the code", true);

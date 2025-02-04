b03f3f73382af025863954286296509e94528fc4
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-04 11:46:12 +0530

    please commit

--- DummyMaven_10/src/test/java/com/bindu/PracticeTest.java
+++ DummyMaven_10/src/test/java/com/bindu/PracticeTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class PracticeTest {
 @Test
-public void pillAndForkReguest() {
+public void pullAndForkReguest() {
 	Reporter.log("please add the code", true);
 }
 
